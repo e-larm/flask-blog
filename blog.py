a6f21dcc8d784ea5439da19b3118ce9bbd960359
Mwirigi Eric <mwirigieric@gmail.com>
2018-08-02 15:56:54 +0300

    init

--- blog.py
+++ blog.py
@@ -4,6 +4,8 @@
 from flask import Flask, render_template, request, session, \
       flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
+
 
 # configuration
 DATABASE = 'blog.db'
@@ -20,6 +22,16 @@ app.config.from_object(__name__)
 def connect_db():
   return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+  @wraps(test)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return test(*args, **kwargs)
+    else:
+      flash('You need to log in first.')
+      return redirect(url_for('login'))
+  return wrap
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
   error = None
@@ -34,15 +46,16 @@ def login():
       return redirect(url_for('main'))
   return render_template('login.html', error=error), status_code
 
+@app.route('/main')
+@login_required
+def main():
+  return render_template('main.html')
+
 @app.route('/logout')
 def logout():
   session.pop('logged_in', None)
   flash('You were logged out')
   return redirect(url_for('login'))
-
-@app.route('/main')
-def main():
-  return render_template('main.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
